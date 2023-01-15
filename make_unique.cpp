5247b99eeacc0961e578e09ad1b1b6ff50cf1003
Repo-Courses <118922298+Repo-Courses@users.noreply.github.com>
2023-01-15 11:52:43 +0100

    Update make_unique.cpp

--- make_unique.cpp
+++ make_unique.cpp
@@ -7,6 +7,16 @@ void Foo(std::vector<std::unique_ptr<int>>& vec) {
     std::sort(vec.begin(), vec.end(), [](const std::unique_ptr<int>& p1, const std::unique_ptr<int>& p2){ return *p1 < *p2;});
 }
 
+template<typename T>
+std::vector<T> uniqueElements(std::vector<T> input) {
+    std::unordered_set<T> uniqueElements;
+    for (auto& element : input) {
+        uniqueElements.insert(element);
+    }
+    return std::vector<T>(uniqueElements.begin(), uniqueElements.end());
+}
+
+
 int main() {
     std::vector<std::unique_ptr<int>> vec;
     vec.push_back(std::make_unique<int>(5));
@@ -24,5 +34,19 @@ int main() {
     for (const auto& ptr : vec) {
         std::cout << *ptr << " ";
     }
+    
+    int main() {
+    std::vector<int> input = {1, 2, 3, 4, 5, 2, 3, 4, 5};
+    std::vector<int> output = uniqueElements(input);
+    std::cout << "Original vector: ";
+    for (auto& element : input) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "Unique elements vector: ";
+    for (auto& element : output) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
